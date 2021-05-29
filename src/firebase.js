6409eb3fc48b68beece7ddfa582e98ea3d961b6d
lyjelin <u3560393@connect.hku.hk>
2021-05-29 16:18:28 +0800

    #1.1 Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwekrAwWGPNdxw01vRBXJ00LQNyaGhizc",
-    authDomain: "e-twitter-6ee10.firebaseapp.com",
-    projectId: "e-twitter-6ee10",
-    storageBucket: "e-twitter-6ee10.appspot.com",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: "590150286244",
     appId: "1:590150286244:web:6810647e4348d176551f31",
     measurementId: "G-063RRQTXCZ"
