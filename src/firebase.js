738f844c479e3cdb78e5dc0c4400d84dc2d31a7a
lyjelin <u3560393@connect.hku.hk>
2021-05-29 16:05:59 +0800

    #1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwekrAwWGPNdxw01vRBXJ00LQNyaGhizc",
+    authDomain: "e-twitter-6ee10.firebaseapp.com",
+    projectId: "e-twitter-6ee10",
+    storageBucket: "e-twitter-6ee10.appspot.com",
+    messagingSenderId: "590150286244",
+    appId: "1:590150286244:web:6810647e4348d176551f31",
+    measurementId: "G-063RRQTXCZ"
+  };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
+
