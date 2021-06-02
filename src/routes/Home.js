f0fb9023b4713fb35f336c5b02f272a07866256e
lyjelin <u3560393@connect.hku.hk>
2021-06-02 23:20:53 +0800

    #3.0 Forms and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <span>Home</span>;
+const Home = () => {
+    const [eweet, setEweet] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const onChange = (event) => {
+        const { target:{value}, 
+    } = event;
+    setEweet(value);
+    }
+    return (
+        <div>
+            <form>
+                <input value={eweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="e.twitter" />
+            </form>
+        </div>
+    );
+};
+
+export default Home;
