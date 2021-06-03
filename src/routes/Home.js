7f1d91919be89a17bb476b92d4bb5993e330e54a
lyjelin <u3560393@connect.hku.hk>
2021-06-04 00:26:02 +0800

    #3.1 Nweeting! - but seems like there is an error in firestore :(

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 const Home = () => {
@@ -5,16 +6,22 @@ const Home = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        dbService.collection("eweets").add({
+            eweet, 
+            createdAt: Date.now(),  
+        });
+        setEweet("");
     };
 
     const onChange = (event) => {
-        const { target:{value}, 
-    } = event;
-    setEweet(value);
-    }
+        const { 
+            target:{ value }, 
+        } = event;
+        setEweet(value);
+    };
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input value={eweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="e.twitter" />
             </form>
