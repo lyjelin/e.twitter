124fb7e3d407ab5501115ee3db1f6634e02a8ca9
lyjelin <u3560393@connect.hku.hk>
2021-06-14 22:09:34 +0800

    #3.2 Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [eweet, setEweet] = useState("");
+    const [eweets, setEweets] = useState("");
+    const getEweets = async() => {
+        const dbEweets = await dbService.collection("eweets").get();
+        dbEweets.forEach((document)=> {
+            const eweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setEweets((prev) => [eweetObject, ...prev]);
+        });
+    };
+
+    useEffect(() => {
+        getEweets();
+    }, [])
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -25,6 +40,13 @@ const Home = () => {
                 <input value={eweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="e.twitter" />
             </form>
+            <div>
+                {eweets.map((eweet) => (
+                    <div key={eweet.id}>
+                         <h4>{eweet.eweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
