a1427efe37eef6ff348bdf1a44e8c04c5cf236d9
lyjelin <u3560393@connect.hku.hk>
2021-06-14 23:04:09 +0800

    #3.4 Delete and Update part Two

--- src/components/Eweet.js
+++ src/components/Eweet.js
@@ -1,14 +1,58 @@
 import React from "react";
+import { dbService } from "fbase";
 
 const Eweet = ({eweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newEweet, setNewEweet] = useState(eweetObj.text);
+    const onDeleteClick = () => {
+        const ok = window.confirm("Are you sure you want to delete this eweet?");
+        if (ok) {
+            dbService.doc(`eweets/${eweetObj.id}`).delete();
+        }
+    }
+    
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        console.log(eweetObj, newEweet);
+        await dbService.doc(`eweets/${eweetObj.id}`).update({
+            text:newEweet,
+        });
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewEweet(value);
+    };
+    return(
     <div>
-        <h4>{nweetObj.text}</h4>
-        {isOwnerr && <> 
-        <button>Delete Eweet</button>
-        <button>Edit Eweet</button>
-        </>}
+        {editing ? (
+            <>
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="Edit your eweet" 
+                    value={newEweet} 
+                    onChange={onChange}
+                    required />
+                <input type="submit" value="Update Eweet" />
+            </form>  
+            <button onClick={toggleEditing}>Cancel</button>
+            </>
+        ) : (
+        <>
+            <h4>{eweetObj.text}</h4>
+            {isOwnerr && (
+                <> 
+                    <button onClick={onDeleteClick}>Delete Eweet</button>
+                    <button onClick={toggleEditing}>Edit Eweet</button>
+                </>
+            )}</>
+        }
     </div>
-
+    );
 };
 
 export default Eweet;
