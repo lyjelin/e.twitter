fe21b240c6b0fad1e8cd62f8feadba75a46965b7
lyjelin <u3560393@connect.hku.hk>
2021-06-16 22:43:06 +0800

    #5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -4,23 +4,36 @@ import { useHistory } from "react-router-dom";
 
 export default ({ userObj }) => {
     const history = useHistory();
+    const [ newDisplayName, setNewDisplayName ] = useState(userObj.newDisplayName);
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
-    const getMyEweets = async() => {
-        const eweets = await dbService
-            .collection("eweets")
-            .where("creatorId", "==", userObj.uid)
-            .orderBy("createdAt")
-            .get();
+    const onChange = (event) => {
+        const {
+           target: { value },
+        } = event;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+        }
     };
+    
     useEffect(() => {
         getMyEweets();
     }, []);
 
     return (
         <>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Display name" />
+            <input type="submit" value="Update Profile" />
+        </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
